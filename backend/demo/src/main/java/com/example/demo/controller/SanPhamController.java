75c4e03e5ea3154f2b10265412004dd7167c2662
tgiabao1340 <39157595+tgiabao1340@users.noreply.github.com>
2019-04-18 17:28:59 +0700

    chinh sua nho

--- backend/demo/src/main/java/com/example/demo/controller/SanPhamController.java
+++ backend/demo/src/main/java/com/example/demo/controller/SanPhamController.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.model.SanPham;
@@ -51,7 +52,7 @@ public class SanPhamController {
 		return "sanpham";
 	}
 	
-	@RequestMapping(value = "/sanpham/{id}")
+	@RequestMapping(value = "/sanpham/{id}", method = RequestMethod.GET)
 	public String getChitietSanPham(Model model, @PathVariable String maSanPham) {
 		model.addAttribute("sanpham", sanPhamRepository.findById(maSanPham));
 		return "chitiet-sanpham";
